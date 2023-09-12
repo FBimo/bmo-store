d67abf11cd2b6af5257703859ff92b32d636d063
fbimo <f.bimo31@gmail.com>
2023-09-12 14:40:48 +0700

    add my_app variable for app title

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'my_app': 'Bmo Store',
         'name': 'FBmo',
         'class': 'PBP C'
     }
